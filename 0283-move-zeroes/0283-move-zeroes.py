ca48a0fabed6d759c04e491e6661fcc0e81eeed4
Mallory Sorola <127247712+MalloryAnn@users.noreply.github.com>
2026-09-01 08:19:28 -0500

    Time: 3 ms (90.34%), Space: 13.6 MB (19.95%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        write = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[write] = nums[i]
+                write +=1
+        for i in range(write, len(nums)):
+            nums[i] = 0
+
+        
